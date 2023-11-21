79b6ecf6ff828759e289db994339c0eb87f8e437
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-11-21 15:20:36 -0500

    update link to github, readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,20 @@
     import REPL from '$lib/REPL.svelte';
 </script>
 
-<h1>Archie's Silly Markdown REPL</h1>
+<h1>Silly Markdown REPL</h1>
+
+<p>The code for this REPL is in <a href="https://github.com/archiewood/silly-repl">this repo</a>.</p>
 
 <REPL/>
 
 
 <style>
     h1 {
+        /* center */
+        text-align: center;
+    }
+
+    p {
         /* center */
         text-align: center;
     }
