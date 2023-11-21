a1a87b01443b8447f00f7e5d2f4d15ba28be01d7
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-11-21 13:53:45 -0500

    initial commit

---
+++ src/lib/REPL.svelte
@@ -0,0 +1,50 @@
+<script>
+    let inputCode = '';
+    let outputHtml = '';
+  
+    const compile = (input) => {
+      // Placeholder for the compile function
+      // You can replace this with your actual compilation logic
+      return input;
+    };
+  
+    const evaluateCode = () => {
+      try {
+        outputHtml = compile(inputCode);
+      } catch (error) {
+        outputHtml = `Error: ${error.message}`;
+      }
+    };
+  </script>
+  
+  <style>
+    main {
+      display: flex;
+      gap: 20px;
+    }
+  
+    textarea {
+      flex: 1;
+      height: 200px;
+    }
+  
+    button {
+      flex: 0;
+    }
+  
+    .output-container {
+      flex: 1;
+    }
+  </style>
+  
+  <main>
+    <textarea bind:value={inputCode} placeholder="Enter HTML code"></textarea>
+    <button on:click={evaluateCode}>Run</button>
+    {#if outputHtml}
+      <div class="output-container">
+        <h3>Output:</h3>
+        {@html outputHtml}
+      </div>
+    {/if}
+  </main>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import REPL from '$lib/REPL.svelte';
+</script>
+
+<h1>Archie's Silly REPL</h1>
+
+<REPL/>
